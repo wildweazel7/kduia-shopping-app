bcad1286343790472799f3511a7ffa86cb818939
Michael Schnabel <wildweazel7@gmail.com>
2023-07-10 11:10:08 -0400

    modification of components - it workded as it should :)

--- src/App.js
+++ src/App.js
@@ -8,8 +8,36 @@ import Location from './components/Location';
 
 function App() {
   return (
+    <AppProvider>
     <div className="App">
+        <div className="row mt-2">
+            {
+                <div className="col-sm">
+                    <CartValue />
+                </div>
+            }
+            {
+                <div className="col-sm">
+                    <Location />
+                </div>
+            }
+        </div>
+        <div className="row mt-2">
+        {
+                <div className="col-sm">
+                    <ExpenseList />
+                </div>
+        }
+        </div>
+        <div className="row mt-2">
+        {
+                <div className="col-sm">
+                    <ItemSelected />
+                </div>
+        }   
+        </div>
     </div>
+    </AppProvider>
   );
 }
 
